6a499a13726aa256e1c49f0b434289dc02af8b62
Glory ann Conwi <gaconwi@googlemail.com>
2023-12-19 12:36:57 +0100

    Challenge: Backend basics routes

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 import { createServer } from "node:http";
 
-// export const server = …
+export const server = createServer((request, response) => {
+    if(request.url === "/api/fish/1") {
+        const answer1 = "Shrimp";
+        response.statusCode = 200;
+        response.end(answer1);
+        return;
+    }
+    if(request.url === "/api/fish/2") {
+        const answer2 = "Anemonefish";
+        response.statusCode = 200;
+        response.end(answer2);
+        return;
+    }
+    response.statusCode = 404;
+    response.end("Not found");
+})
+
+export default server;
